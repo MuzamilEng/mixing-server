c99b3578fbf87007b7d4bdedb81c610a3dcdaf4d
MuzamilEng <muzekhan777@gmail.com>
2026-04-23 04:14:32 +0500

    fixed prisma

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,4 +1,3 @@
 // Use the root project's generated Prisma client (generated via root prisma generate)
-import { PrismaClient } from '../../node_modules/@prisma/client';
-
+import { PrismaClient } from '@prisma/client';
 export const prisma = new PrismaClient();
